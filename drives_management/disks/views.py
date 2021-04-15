de8803e6f96173ed7dbf5e2f817ad13c2eac7e34
TunaLagoona <maslova.wrk@gmail.com>
2021-04-15 17:21:12 +0100

    Add POST request processing

--- drives_management/disks/views.py
+++ drives_management/disks/views.py
@@ -36,3 +36,29 @@ class View1(TemplateView):
             'sdb_partitions': sdb_partitions
         }
         return render(request, 'disks.html', context=context)
+
+    def post(self, request, *args, **kwargs):
+        print(f'request.POST: {request.POST}')
+        if request.POST["disk"] == 'sda1' or request.POST["disk"] == 'sda2':
+            print('no action will be taken')
+        else:
+            if request.POST["command"] == 'Mount':
+                letters = string.ascii_lowercase
+                dir_name = ''.join(random.choice(letters) for i in range(4))
+                print(f'new directory: {dir_name}')
+                mkdir = 'sudo mkdir /mnt/' + dir_name.strip()
+                process = subprocess.Popen([mkdir], stdout=subprocess.PIPE, shell=True)
+                out, err = process.communicate()
+                print(f'mkdir out: {out}, err: {err}')
+                cmnd = 'sudo mount ' + '/dev/' + request.POST["disk"].strip() + ' /mnt/' + dir_name.strip()
+                print(f'command: {cmnd}')
+            elif request.POST["command"] == 'Unmount':
+                cmnd = 'sudo umount ' + '/dev/' + request.POST["disk"].strip()
+                process = subprocess.Popen([cmnd], stdout=subprocess.PIPE, shell=True)
+                out, err = process.communicate()
+                print(f'command: {cmnd}')
+                print(f'out: {out}, err: {err}')
+            elif request.POST["command"] == 'Format':
+                cmnd = 'sudo mkfs.xfs -f ' + '/dev/' + request.POST["disk"].strip()
+                print(f'command: {cmnd}')
+        return self.get(request)
