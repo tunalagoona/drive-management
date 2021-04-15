c61b021529eefe106cc7fd50016b76c0ca222576
TunaLagoona <maslova.wrk@gmail.com>
2021-04-15 20:20:23 +0100

    Refactor according to SoC principle

--- drives_management/disks/views.py
+++ drives_management/disks/views.py
@@ -1,4 +1,5 @@
 import random
+from typing import List, Dict
 
 from django.shortcuts import render
 import subprocess
@@ -14,9 +15,39 @@ from django.urls import reverse
 decorators = [never_cache, login_required]
 
 
-@method_decorator(decorators, name='dispatch')
-class View1(TemplateView):
-    def get(self, request, *args, **kwargs):
+class DiskManagement:
+    @staticmethod
+    def mount(disk) -> None:
+        letters = string.ascii_lowercase
+        dir_name = "".join(random.choice(letters) for i in range(4))
+        mkdir = "sudo mkdir /mnt/" + dir_name.strip()
+        mkdir_process = subprocess.Popen([mkdir], stdout=subprocess.PIPE, shell=True)
+        out, err = mkdir_process.communicate()
+        print(f"mkdir out: {out}, err: {err}")
+        cmnd = "sudo mount " + "/dev/" + disk + " /mnt/" + dir_name.strip()
+        print(f"command: {cmnd}")
+        mount_process = subprocess.Popen([cmnd], stdout=subprocess.PIPE, shell=True)
+        out, err = mount_process.communicate()
+        print(f"mount out: {out}, err: {err}")
+
+    @staticmethod
+    def unmount(disk) -> None:
+        cmnd = "sudo umount " + "/dev/" + disk
+        process = subprocess.Popen([cmnd], stdout=subprocess.PIPE, shell=True)
+        out, err = process.communicate()
+        print(f"command: {cmnd}")
+        print(f"out: {out}, err: {err}")
+
+    @staticmethod
+    def format(disk) -> None:
+        cmnd = "sudo mkfs.xfs -f " + "/dev/" + disk
+        print(f"command: {cmnd}")
+        process = subprocess.Popen([cmnd], stdout=subprocess.PIPE, shell=True)
+        out, err = process.communicate()
+        print(f"out: {out}, err: {err}")
+
+    @staticmethod
+    def list_block_devices() -> List[Dict]:
         """Dividing lsblk into a core library (*.so) and binary is not supported yet
         so a new subprocess is created instead."""
         process = subprocess.Popen(
@@ -25,24 +56,28 @@ class View1(TemplateView):
             shell=True,
         )
         out, err = process.communicate()
-        attributes = []
-
-        res = out.splitlines()
-
-        for disk in res:
-            d_attributes = disk.split()
+        output = out.splitlines()
+        disks_and_partitions = []
+        for line in output:
+            d_attributes = line.split()
             disk_name = d_attributes[0].decode("utf-8").strip()
             disk_size = d_attributes[1].decode("utf-8").strip()
             disk_mountpoint = ""
             if len(d_attributes) > 2:
                 disk_mountpoint = d_attributes[2].decode("utf-8").strip()
             attr = {"name": disk_name, "size": disk_size, "mountpoint": disk_mountpoint}
-            attributes.append(attr)
+            disks_and_partitions.append(attr)
+        return disks_and_partitions
 
-        sda_partitions = attributes[1:6]
-        sdb_partitions = attributes[7:]
 
-        context = {"disks": attributes, "sda_partitions": sda_partitions, "sdb_partitions": sdb_partitions}
+@method_decorator(decorators, name="dispatch")
+class View1(TemplateView):
+    def get(self, request, *args, **kwargs):
+        dm = DiskManagement()
+        disks: List[Dict] = dm.list_block_devices()
+        sda_partitions = disks[1:6]
+        sdb_partitions = disks[7:]
+        context = {"sda_partitions": sda_partitions, "sdb_partitions": sdb_partitions}
         return render(request, "disks.html", context=context)
 
     def post(self, request, *args, **kwargs):
@@ -51,31 +86,11 @@ class View1(TemplateView):
             print("no action will be taken")
         else:
             disk = request.POST["disk"].strip()
+            dm = DiskManagement()
             if request.POST["command"] == "Mount":
-                letters = string.ascii_lowercase
-                dir_name = "".join(random.choice(letters) for i in range(4))
-                mkdir = "sudo mkdir /mnt/" + dir_name.strip()
-                mkdir_process = subprocess.Popen([mkdir], stdout=subprocess.PIPE, shell=True)
-                out, err = mkdir_process.communicate()
-                print(f"mkdir out: {out}, err: {err}")
-                cmnd = "sudo mount " + "/dev/" + disk + " /mnt/" + dir_name.strip()
-                print(f"command: {cmnd}")
-                mount_process = subprocess.Popen([cmnd], stdout=subprocess.PIPE, shell=True)
-                out, err = mount_process.communicate()
-                print(f"mount out: {out}, err: {err}")
-
+                dm.mount(disk)
             elif request.POST["command"] == "Unmount":
-                cmnd = "sudo umount " + "/dev/" + disk
-                process = subprocess.Popen([cmnd], stdout=subprocess.PIPE, shell=True)
-                out, err = process.communicate()
-                print(f"command: {cmnd}")
-                print(f"out: {out}, err: {err}")
-
+                dm.unmount(disk)
             elif request.POST["command"] == "Format":
-                cmnd = "sudo mkfs.xfs -f " + "/dev/" + disk
-                print(f"command: {cmnd}")
-                process = subprocess.Popen([cmnd], stdout=subprocess.PIPE, shell=True)
-                out, err = process.communicate()
-                print(f"out: {out}, err: {err}")
-
-        return HttpResponseRedirect(reverse('disks'))
+                dm.format(disk)
+        return HttpResponseRedirect(reverse("disks"))
